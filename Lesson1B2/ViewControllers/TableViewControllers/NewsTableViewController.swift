8f6d089f97e9b421dd0272d4a89c9097e89ed5e3
Николай Унтевский <untewsky@yandex.ru>
2021-07-15 13:44:29 +0300

    Новость теперь отображаeтся не одной ячейкой, как раньше, а одной секцией. Внутри секции новости теперь несколько типов и прототипов ячеек, например: ячейка текста новости, ячейка фотографии новости, ячейка с автором и временем публикации, ячейка с количеством лайков и комментариев.

--- Lesson1B2/ViewControllers/TableViewControllers/NewsTableViewController.swift
+++ Lesson1B2/ViewControllers/TableViewControllers/NewsTableViewController.swift
@@ -18,29 +18,31 @@ class NewsTableViewController: UITableViewController {
         let new5 = News(source: DataStorage.shared.myGroups[4], text: "Арт-группа Instigators и художник Николай Кошелев запустили NFT-акцию «Экспортировано из музея» В основу акции легли работы с выставки Николая Кошелева в Третьяковской галерее. Художник переработал два произведения и создал восемь NFT. Instigators разместили токены на Rarible. 21 апреля пользователь платформы купил один из лотов за 1,01 WETH ($2480). На момент публикации за NFT предлагают от 0,003 WETH ($7) до 0,5 WETH ($1230). Торги продлятся до 25 апреля. Вырученные средства пойдут на новые проекты арт-группы и художника.«Потенциал блокчейна колоссален и может изменить привычный ход вещей на арт-рынке. Наша акция позволяет приобрести работы, экспортированные из физической реальности музейного пространства в цифровой мир», — комментирует основатель Instigators Денис Давыдов.", image: UIImage(named:"theMarketNew"))
         let new6 = News(source: DataStorage.shared.myGroups[1], text: "", image: UIImage(named: "Никита"))
         DataStorage.shared.news.append(contentsOf: [new5, new2, new4, new6, new1, new3])
+//        DataStorage.shared.news.append(contentsOf: [new5])
     }
     
     var key = 0
     var viewing = [Int:Int]()
     var liked = [Int:Bool]()
     var views = 0
+    var newsTextHeight = CGFloat()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         fillTheNews()
+        tableView.delegate = self
+        tableView.dataSource = self
         
-        self.tableView.rowHeight = UITableView.automaticDimension
-        self.tableView.estimatedRowHeight = 44
-        
-        for _ in DataStorage.shared.news {
-            viewing[key] = 0
-            liked[key] = false
-            key += 1
-        }
-        
-        let nibFile = UINib(nibName: "NewsTableViewCell", bundle: nil)
-        tableView.register(nibFile, forCellReuseIdentifier: "News")
+        let nibFile1 = UINib(nibName: "NewsSourceCell", bundle: nil)
+        tableView.register(nibFile1, forCellReuseIdentifier: "NewsSourceCell")
+        let nibFile2 = UINib(nibName: "NewsTextCell", bundle: nil)
+        tableView.register(nibFile2, forCellReuseIdentifier: "NewsTextCell")
+        let nibFile3 = UINib(nibName: "NewsImageCell", bundle: nil)
+        tableView.register(nibFile3, forCellReuseIdentifier: "NewsImageCell")
+        let nibFile4 = UINib(nibName: "NewsLikesSharingCell", bundle: nil)
+        tableView.register(nibFile4, forCellReuseIdentifier: "NewsLikesSharingCell")
+        self.tableView.register(NewsImageTableViewCell.self, forCellReuseIdentifier: "NewsImageTableViewCell")
         
     }
 
@@ -50,35 +52,74 @@ class NewsTableViewController: UITableViewController {
     
     // MARK: - Table view data source
 
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         return DataStorage.shared.news.count
     }
-
-  
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "News") as! NewsTableViewCell
-        
-        views += 1
-        if views < 3 {
-            viewing[indexPath.row]! += 1
+        switch indexPath.row {
+        case 0:
+            let newsSourceCell = tableView.dequeueReusableCell(withIdentifier: "NewsSourceCell", for: indexPath) as! NewsSourceCell
+            newsSourceCell.configure(sorsName: DataStorage.shared.news[indexPath.section].source.name, sorsImage: DataStorage.shared.news[indexPath.section].source.image)
+            return newsSourceCell
+        case 1:
+            let newsTextCell = tableView.dequeueReusableCell(withIdentifier: "NewsTextCell", for: indexPath) as! NewsTextCell
+            newsTextCell.configure(newText: DataStorage.shared.news[indexPath.section].text)
+            return newsTextCell
+        case 2:
+            let newsImageCell = tableView.dequeueReusableCell(withIdentifier: "NewsImageTableViewCell", for: indexPath) as! NewsImageTableViewCell
+            newsImageCell.configure(newImage: DataStorage.shared.news[indexPath.section].image)
+            return newsImageCell
+        case 3:
+            let newsLikesSharingCell = tableView.dequeueReusableCell(withIdentifier: "NewsLikesSharingCell", for: indexPath) as! NewsLikesSharingCell
+            return newsLikesSharingCell
+        default:
+            let newsLikesSharingCell = tableView.dequeueReusableCell(withIdentifier: "NewsLikesSharingCell", for: indexPath) as! NewsLikesSharingCell
+            return newsLikesSharingCell
         }
-        
-        cell.configure(
-            
-            sorsName: DataStorage.shared.news[indexPath.row].source.name,
-            newText: DataStorage.shared.news[indexPath.row].text,
-            sorsImage: DataStorage.shared.news[indexPath.row].source.image,
-            newImage: DataStorage.shared.news[indexPath.row].image)
-        cell.viewingLabel.text = "\(viewing[indexPath.row]!)"
-        viewing[indexPath.row]! += 1
-//        liked[indexPath.row]! = cell.isLiked
-//        cell.isLiked = false
-        return cell
-        
+
     }
     
 
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        switch indexPath.row {
+        case 0:
+
+            return self.tableView.rowHeight
+        case 1:
+            let newsText = DataStorage.shared.news[indexPath.section].text
+            if newsText != "" {
+                return self.tableView.rowHeight
+            } else {
+                return 0
+            }
+        case 2:
+            
+            guard let currentImage =  DataStorage.shared.news[indexPath.section].image else { return 0 }
+            let imageRatio = currentImage.getImageRatio()
+            return tableView.frame.width / imageRatio
+        case 3:
+
+            return self.tableView.rowHeight
+        default:
+            return 0
+        }
+
+    }
+}
+
+extension UIImage {
+    func getImageRatio() -> CGFloat {
+        let imageRatio = CGFloat(self.size.width / self.size.height)
+        return imageRatio
+    }
 }
